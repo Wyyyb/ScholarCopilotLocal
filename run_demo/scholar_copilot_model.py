20b33013b72fbc6d7c4b8ee73e9429651d916bcf
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-27 00:56:17 +0800

    add

--- run_demo/scholar_copilot_model.py
+++ run_demo/scholar_copilot_model.py
@@ -236,7 +236,7 @@ def replace_citations(input_text, reference_id_list, citation_map):
     # Replace all citations
     result = re.sub(pattern, replace_match, input_text)
     result = result.replace("<|paper_start|> ", "").replace("<|cite_start|>", "")
-    print("res_citation_data_list", res_citation_data_list)
+    print("res_citation_data_list", res_citation_data_list[:1])
 
     return result, res_citation_data_list
 
