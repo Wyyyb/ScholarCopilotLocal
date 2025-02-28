aa1622b138dc46c119e40159421f27ec2a84e6fc
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-28 21:44:41 +0800

    add

--- evaluation/evaluate_qwen_2-5_72b_instruct_generation_0.sh
+++ evaluation/evaluate_qwen_2-5_72b_instruct_generation_0.sh
@@ -2,6 +2,6 @@
 source /map-vepfs/miniconda3/bin/activate
 conda activate yubo_lf
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python evaluate_qwen_2-5_72b_instruct_generation_0226.py
